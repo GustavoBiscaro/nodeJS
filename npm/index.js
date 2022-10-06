414dd63f37571e21414b2e39ff3c36477f11efb1
Gustavo <gustavo.biscaro@gmail.com>
2022-10-05 22:05:34 -0300

    Aula 08 - Parâmetros

--- npm/index.js
+++ npm/index.js
@@ -13,6 +13,9 @@ app.get("/blog", function(req, res){
     res.send("Seja bem-vindo ao meu blog!");
 });
 
+app.get('/ola/:nome/:cargo/:nacionalidade', function(req, res){
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2>Seu cargo: " + req.params.cargo + "</h2>" + "<h3> Sua nacionalidade: " +req.params.nacionalidade + "</h3>");
+});
 
 
 app.listen(8081, function(){
