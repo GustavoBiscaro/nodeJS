25081c0ef9c4d189a721aec09df22d858b5685c5
Gustavo <gustavo.biscaro@gmail.com>
2022-10-12 00:55:22 -0300

    Trabalhando com Sequelize

---
+++ npm/teste.js
@@ -0,0 +1,44 @@
+﻿const Sequelize = require('sequelize')
+const sequelize = new Sequelize('test','root','mzLA18@#*', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.create({
+    titulo: "Sobre Nós",
+    conteudo: "Uma empresa que está a anos no mercado!"
+})
+
+
+const Usuario = sequelize.define('usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: "Gustavo",
+    sobrenome: "Miguel",
+    idade: 25,
+    email: "gustavo.biscaro@hotmail.com"
+})
+
+//Usuario.sync({force: true})
