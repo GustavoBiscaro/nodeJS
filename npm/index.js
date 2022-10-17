e341ff5889c3a1b4834a3082bdd8a0024523ce44
Gustavo Miguel <gustavo.biscaro@gmail.com>
2022-10-17 15:07:33 -0300

    Enviando formulário

--- npm/index.js
+++ npm/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 // Config
     // Template Engine
     app.engine("handlebars", handlebars.engine({defaultLayout: "main"}))
     app.set('view engine', 'handlebars')
+
+    // Body Parser
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
 
     // Conexão com o banco de dados MySQL
     const sequelize = new Sequelize('test','root','mzLA18@#*', {
@@ -19,7 +24,7 @@ const Sequelize = require('sequelize')
     })
     
     app.post('/add', function(req, res){
-        res.send("FORMULARIO RECEBIDO!")
+        res.send("Texto: " + req.body.titulo + " Conteudo: " + req.body.conteudo)
     })
 
 
